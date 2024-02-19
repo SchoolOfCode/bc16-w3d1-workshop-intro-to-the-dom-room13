7cc50f05752786d0322a901b0bd2d43072002443
Jai <shailerj@gmail.com>
2024-02-19 15:04:06 +0100

    Tickets 1c, 1d, and 1e complete

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ console.log(titleElement);
 //Ticket 1b
 let contentClass = document.getElementsByClassName("content");
 console.log(contentClass);
+
+//Ticket 1c
+let listItemElements = document.getElementsByTagName("li");
+console.log(listItemElements);
+
+//Ticket 1d - shouldn't use querySelector as a variable as is not best practice;
+// have used .highlight to find the '.highlight' class in CSS file
+let highlightedElement = document.querySelector(".highlight");
+console.log(highlightedElement);
+
+//Ticket 1e - returns 'NodeList (3)' with all the highlighted elements listed
+let allHighlightedElements = document.querySelectorAll(".highlight")
+console.log(allHighlightedElements);
