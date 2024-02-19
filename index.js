c7cdd657aaa3bcbb79251c7fedc3dc5b3c46b66f
User <esalami19@yahoo.co.uk>
2024-02-19 12:13:11 +0000

    ticket 1b

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ console.log("Linked!");
 
 let titleElement = document.getElementById("title");
 console.log(titleElement);
+
+//Ticket 1b
+let contentClass = document.getElementsByClassName("content");
+console.log(contentClass);
